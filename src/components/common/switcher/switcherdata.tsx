505a46a7d64b056fb8a8c2c0bbcc53dc662fed1f
ZSCMultiverse <101880019+SocialZombie@users.noreply.github.com>
2024-01-25 16:59:25 +0000

    Update switcherData.tsx

--- src/components/common/switcher/switcherdata.tsx
+++ src/components/common/switcher/switcherdata.tsx
@@ -70,21 +70,27 @@ export function Verticaldirection(actionfunction: any) {
   document.querySelector(".main-header")?.classList.remove("hor-header");
   document.querySelector(".main-sidemenu")?.classList.remove("container");
 
-  document.querySelector(".main-content")?.classList.remove("horizontal-content");
-  const myonoffswitch34 = document.getElementById("switchbtn-vertical") as HTMLInputElement;
+  document
+    .querySelector(".main-content")
+    ?.classList.remove("horizontal-content");
+  const myonoffswitch34 = document.getElementById(
+    "switchbtn-vertical"
+  ) as HTMLInputElement;
   myonoffswitch34.checked = true;
   localStorage.setItem("Zemvertical", "vertical");
   localStorage.removeItem("Zemhorizontal");
-  let activeElement : any= document.querySelector(".side-menu__item.active.is-expanded")
-  if(activeElement){
-    activeElement.parentNode.querySelector('ul').style.display = "block"
+  let activeElement: any = document.querySelector(
+    ".side-menu__item.active.is-expanded"
+  );
+  if (activeElement) {
+    activeElement.parentNode.querySelector("ul").style.display = "block";
   }
 }
 // Verticaldirection -End
 
 export function HorizantalClickMenu(actionfunction: any) {
   let horheader = document.querySelector(".main-header") as HTMLElement;
-  if(horheader){
+  if (horheader) {
     horheader.classList.add("hor-header");
   }
   const theme = store.getState();
@@ -97,7 +103,9 @@ export function HorizantalClickMenu(actionfunction: any) {
   });
   document.querySelector(".main-header")?.classList.add("hor-header");
   document.querySelector(".app")?.classList.add("horizontal");
-  document.querySelectorAll(".main-container")[1]?.classList.remove("container-fluid");
+  document
+    .querySelectorAll(".main-container")[1]
+    ?.classList.remove("container-fluid");
   document.querySelectorAll(".main-container")[1]?.classList.add("container");
   document.querySelector(".main-sidemenu")?.classList.add("container");
   document.querySelector(".main-content")?.classList.add("horizontal-content");
@@ -114,7 +122,7 @@ export function HorizantalClickMenu(actionfunction: any) {
   const myonoffswitch35 = document.getElementById(
     "switchbtn-horizontal"
   ) as HTMLInputElement;
-  if(myonoffswitch35){
+  if (myonoffswitch35) {
     myonoffswitch35.checked = true;
   }
   checkHoriMenu();
@@ -124,9 +132,8 @@ export function HorizantalClickMenu(actionfunction: any) {
   localStorage.removeItem("Zemvertical");
   localStorage.removeItem("ZemhorizontalHover");
 
-
-  if(localStorage.zemboxedwidth == "boxed"){
-    document.querySelector("#slide-right")?.classList.remove("d-none")
+  if (localStorage.zemboxedwidth == "boxed") {
+    document.querySelector("#slide-right")?.classList.remove("d-none");
   }
 }
 
@@ -142,7 +149,9 @@ export function HorizantalHoverMenu(actionfunction: any) {
   });
   document.querySelector(".app")?.classList.add("horizontal-hover");
   document.querySelector(".app")?.classList.add("horizontal");
-  document.querySelectorAll(".main-container")[1]?.classList.remove("container-fluid");
+  document
+    .querySelectorAll(".main-container")[1]
+    ?.classList.remove("container-fluid");
   document.querySelectorAll(".main-container")[1]?.classList.add("container");
   document.querySelector(".main-header")?.classList.add("hor-header");
   document.querySelector(".app-sidebar")?.classList.add("horizontal-main");
@@ -183,19 +192,19 @@ export function HorizantalHoverMenu(actionfunction: any) {
   localStorage.setItem("ZemhorizontalHover", "horizontalHover");
   localStorage.removeItem("Zemvertical");
   localStorage.removeItem("Zemhorizontal");
-  if(localStorage.zemboxedwidth == "boxed"){
-    document.querySelector("#slide-right")?.classList.remove("d-none")
+  if (localStorage.zemboxedwidth == "boxed") {
+    document.querySelector("#slide-right")?.classList.remove("d-none");
   }
 }
 
 export function Themedefault(actionfunction: any) {
   const theme = store.getState();
   actionfunction({ ...theme, datathemecolor: "default" });
-  localStorage.setItem("ZemThemestyles", "default");
+  localStorage.setItem("Zemhorizontal", "light");
 }
-export function Themelight(actionfunction: any){
+export function Themelight(actionfunction: any) {
   const theme = store.getState();
-  actionfunction({ ...theme, datathemecolor: "light"});
+  actionfunction({ ...theme, datathemecolor: "light" });
   document.documentElement.style.removeProperty("--background-rgb");
   localStorage.setItem("ZemThemestyles", "light");
   localStorage.removeItem("datathemecolor");
@@ -207,7 +216,7 @@ export function Themedark(actionfunction: any) {
   localStorage.setItem("ZemThemestyles", "dark");
   localStorage.removeItem("datathemecolor");
   document.documentElement.style.removeProperty("--background-rgb");
-  localStorage.removeItem("Dynamicbackground");  
+  localStorage.removeItem("Dynamicbackground");
 }
 export function Themeglassy(actionfunction: any) {
   const theme = store.getState();
@@ -215,7 +224,7 @@ export function Themeglassy(actionfunction: any) {
   localStorage.setItem("ZemThemestyles", "glassy");
   localStorage.removeItem("datathemecolor");
   document.documentElement.style.removeProperty("--background-rgb");
-  localStorage.removeItem("Dynamicbackground");  
+  localStorage.removeItem("Dynamicbackground");
 }
 export const bgImage1 = (actionfunction: any) => {
   const theme = store.getState();
@@ -247,8 +256,8 @@ export const Boxedwidth = (actionfunction: any) => {
   actionfunction({ ...theme, datawidth: "boxed" });
   localStorage.setItem("zemboxedwidth", "boxed");
 
-  if(localStorage.Zemhorizontal == "Zemhorizontal"){
-    document.querySelector("#slide-right")?.classList.remove("d-none")
+  if (localStorage.Zemhorizontal == "Zemhorizontal") {
+    document.querySelector("#slide-right")?.classList.remove("d-none");
   }
 };
 export const Fixed = (actionfunction: any) => {
@@ -279,10 +288,10 @@ function hexToRgb(hex: string) {
   var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
   return result
     ? {
-      r: parseInt(result[1], 16),
-      g: parseInt(result[2], 16),
-      b: parseInt(result[3], 16),
-    }
+        r: parseInt(result[1], 16),
+        g: parseInt(result[2], 16),
+        b: parseInt(result[3], 16),
+      }
     : null;
 }
 const Themeprimarycolor: React.FC = () => {
@@ -348,9 +357,11 @@ export const ResetButton = (actionfunction: any) => {
   document.querySelector(".main-header")?.classList.add("side-header");
   localStorage.clear();
   addOrRemoveCss(ltr);
-  let activeElement : any= document.querySelector(".side-menu__item.active.is-expanded")
-  if(activeElement){
-    activeElement.parentNode.querySelector('ul').style.display = "block"
+  let activeElement: any = document.querySelector(
+    ".side-menu__item.active.is-expanded"
+  );
+  if (activeElement) {
+    activeElement.parentNode.querySelector("ul").style.display = "block";
   }
 };
 // LocalBackup Storage
@@ -410,7 +421,6 @@ export const LocalStorageBackup = (actionfunction: any) => {
 };
 //  switcherArrowFn
 export function switcherArrowFn() {
-
   const slideLeft: any = document.querySelector(".slide-left");
   const slideRight: any = document.querySelector(".slide-right");
   // used to remove is-expanded class and remove class on clicking arrow buttons
@@ -436,7 +446,7 @@ export function switcherArrowFn() {
   }
   // horizontal arrows
   window.addEventListener("resize", () => {
-    if(document.querySelector('.header-icon-svgs')){
+    if (document.querySelector(".header-icon-svgs")) {
       const menuWidth: any = document.querySelector(".horizontal-main");
       const menuItems: any = document.querySelector(".side-menu");
       const mainSidemenuWidth: any = document.querySelector(".main-sidemenu");
@@ -449,8 +459,10 @@ export function switcherArrowFn() {
         Number(window.getComputedStyle(menuItems).marginRight.split("px")[0])
       );
       const check =
-        menuItems.scrollWidth + (0 - menuWidth?.offsetWidth) + menuContainerWidth;
-  
+        menuItems.scrollWidth +
+        (0 - menuWidth?.offsetWidth) +
+        menuContainerWidth;
+
       if (menuWidth?.offsetWidth - menuContainerWidth > menuItems.scrollWidth) {
         document.querySelector(".slide-left")?.classList.add("d-none");
         document.querySelector(".slide-right")?.classList.add("d-none");
@@ -459,7 +471,7 @@ export function switcherArrowFn() {
       } else {
         document.querySelector(".slide-right")?.classList.remove("d-none");
       }
-  
+
       if (document.querySelector("html")?.getAttribute("dir") === "rtl") {
         if (
           Math.abs(marginRightValue) < Math.abs(check) === false &&
@@ -491,7 +503,7 @@ export function switcherArrowFn() {
     checkHoriMenu();
   }
 
-  if(slideLeft && slideRight){
+  if (slideLeft && slideRight) {
     slideLeft.addEventListener("click", () => {
       slideClick();
       const menuWidth: any = document.querySelector(".horizontal-main");
@@ -507,7 +519,7 @@ export function switcherArrowFn() {
         Math.ceil(
           Number(window.getComputedStyle(menuItems).marginRight.split("px")[0])
         ) + 100;
-  
+
       if (document.querySelector("html")?.getAttribute("dir") === "rtl") {
         if (marginRightValue < 0) {
           menuItems.style.marginLeft = "0px";
@@ -518,7 +530,7 @@ export function switcherArrowFn() {
         } else {
           document.querySelector(".slide-left")?.classList.add("d-none");
         }
-  
+
         if (marginRightValue >= 0) {
           menuItems.style.marginLeft = "0px";
           menuItems.style.marginRight = "0px";
@@ -546,14 +558,14 @@ export function switcherArrowFn() {
         } else {
           document.querySelector(".slide-left")?.classList.add("d-none");
         }
-  
+
         if (marginLeftValue >= 0) {
           menuItems.style.marginLeft = "0px";
           if (menuWidth?.offsetWidth < menuItems.scrollWidth) {
             document.querySelector(".slide-left")?.classList.add("d-none");
           }
         }
-  
+
         // to remove dropdown when clicking arrows in horizontal menu
         const subNavSub = document.querySelectorAll(".sub-nav-sub");
         subNavSub.forEach((e: any) => {
@@ -581,8 +593,10 @@ export function switcherArrowFn() {
           Number(window.getComputedStyle(menuItems).marginRight.split("px")[0])
         ) - 100;
       const check =
-        menuItems.scrollWidth + (0 - menuWidth?.offsetWidth) + menuContainerWidth;
-  
+        menuItems.scrollWidth +
+        (0 - menuWidth?.offsetWidth) +
+        menuContainerWidth;
+
       if (document.querySelector("html")?.getAttribute("dir") === "rtl") {
         if (marginRightValue > -check) {
           menuItems.style.marginLeft = "0px";
@@ -594,7 +608,7 @@ export function switcherArrowFn() {
           document.querySelector(".slide-right")?.classList.add("d-none");
           document.querySelector(".slide-left")?.classList.remove("d-none");
         }
-  
+
         if (marginRightValue !== 0) {
           document.querySelector(".slide-left")?.classList.remove("d-none");
         }
@@ -635,11 +649,9 @@ export function switcherArrowFn() {
   }
 }
 
-
-
 //  switcherArrowFn -End
 export function checkHoriMenu() {
-  if(document.querySelector('.header-icon-svgs')){
+  if (document.querySelector(".header-icon-svgs")) {
     const menuWidth: any = document.querySelector(".horizontal-main");
     const menuItems: any = document.querySelector(".side-menu");
     const mainSidemenuWidth: any = document.querySelector(".main-sidemenu");
@@ -658,7 +670,10 @@ export function checkHoriMenu() {
     } else {
       menuItems.style.marginLeft = 0;
     }
-    if (menuItems.scrollWidth - 2 < menuWidth?.offsetWidth - menuContainerWidth) {
+    if (
+      menuItems.scrollWidth - 2 <
+      menuWidth?.offsetWidth - menuContainerWidth
+    ) {
       document.querySelector(".slide-left")?.classList.add("d-none");
       document.querySelector(".slide-right")?.classList.add("d-none");
       document.querySelector(".slide-leftRTL")?.classList.add("d-none");
@@ -670,7 +685,10 @@ export function checkHoriMenu() {
       document.querySelector(".slide-left")?.classList.remove("d-none");
       document.querySelector(".slide-leftRTL")?.classList.remove("d-none");
     }
-    if (menuItems.scrollWidth - 2 > menuWidth?.offsetWidth - menuContainerWidth) {
+    if (
+      menuItems.scrollWidth - 2 >
+      menuWidth?.offsetWidth - menuContainerWidth
+    ) {
       document.querySelector(".slide-left")?.classList.remove("d-none");
       document.querySelector(".slide-right")?.classList.remove("d-none");
       document.querySelector(".slide-leftRTL")?.classList.remove("d-none");

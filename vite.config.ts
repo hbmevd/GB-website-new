67bc32ec6d4d2f36e535d7ba852a63c76e98e6ab
ZSCMultiverse <101880019+SocialZombie@users.noreply.github.com>
2024-01-25 14:27:07 +0000

    Push init

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/zem-ts/preview/",
   // base: "/test/react-zem-ts1/dist/",
   plugins: [react()],
   server: {
